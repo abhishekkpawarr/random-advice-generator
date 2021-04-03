cd07cadffd6590ecfe4bd258cd672b9930c46a84
abhishek <64072630+abhishekpawar1402@users.noreply.github.com>
2021-04-03 06:13:04 +0530

    chenge font size

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     return (
       <div className="app">
         <div className="card">
-          <h3 className="heading">{advice}</h3>
+          <h2 className="heading">{advice}</h2>
           <button className="button" onClick={this.fetchAdvice}>
             <span>GIVE ME ADVICE!</span>
           </button>
