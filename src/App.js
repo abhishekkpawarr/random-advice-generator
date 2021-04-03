667933f25b1d495da3216cb431f6aad413b24d93
abhishek <64072630+abhishekpawar1402@users.noreply.github.com>
2021-04-03 06:37:30 +0530

    changed font size

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
     return (
       <div className="app">
         <div className="card">
-          <h2 className="heading">{advice}</h2>
+          <h3 className="heading">{advice}</h3>
           <button className="button" onClick={this.fetchAdvice}>
             <span>GIVE ME ADVICE!</span>
           </button>
